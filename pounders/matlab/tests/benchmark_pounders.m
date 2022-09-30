e616148eaa89dadb672ab6903028ec3944fb0d7e
Jeffrey Larson <jmlarson@anl.gov>
2022-09-30 16:01:05 -0500

    Pounders must receive the fvec value, not the scalar value

--- pounders/matlab/tests/benchmark_pounders.m
+++ pounders/matlab/tests/benchmark_pounders.m
@@ -57,7 +57,7 @@ for row = 1:53
     printf = 0;
 
 
-    [X,F,flag,xkin] = pounders(@calfun,X0,n,npmax,nfmax,gtol,delta,nfs,m,F0,xkin,L,U,printf);
+    [X,F,flag,xkin] = pounders(@calfun_wrapper,X0,n,npmax,nfmax,gtol,delta,nfs,m,F0,xkin,L,U,printf);
 
     assert(size(X,1) <= nfmax, "POUNDERs grew the size of X")
     if re_check
@@ -77,3 +77,8 @@ for row = 1:53
 %     save('-mat7-binary', filename, 'Results') % Octave save
 end
 save(filename, 'Results');
+end
+
+function [fvec] = calfun_wrapper(x)
+   [~, fvec, ~] = calfun(x);
+end
