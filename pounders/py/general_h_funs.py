1b425cc2fb16eace3f02b7c37f9cc0e474974a60
Jeffrey Larson <jmlarson@anl.gov>
2022-10-31 11:58:49 -0500

    Adding a new hfun

--- pounders/py/general_h_funs.py
+++ pounders/py/general_h_funs.py
@@ -1,6 +1,19 @@
 import numpy as np
 
 
+def neg_leastsquares(Cres, Gres, Hres):
+    n, _, m = Hres.shape
+
+    G = -2 * Gres @ Cres.T
+    H = np.zeros((n, n))
+    for i in range(m):
+        H = H + Cres[i] * Hres[:, :, i]
+
+    H = -2 * H - 2 * Gres @ Gres.T
+
+    return G, H
+
+
 def leastsquares(Cres, Gres, Hres):
     n, _, m = Hres.shape
 
