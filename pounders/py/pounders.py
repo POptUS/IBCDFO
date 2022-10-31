cd3ef3587f00d94523002e7724836870b8094126
Jeffrey Larson <jmlarson@anl.gov>
2022-10-31 11:46:53 -0500

    Varying level verbosity for printing to screen

--- pounders/py/pounders.py
+++ pounders/py/pounders.py
@@ -43,7 +43,9 @@ def pounders(fun, X0, n, mpmax, nfmax, gtol, delta, nfs, m, F0, xkin, L, U, prin
     # xkin    [int] Index of point in X0 at which to start from (1)
     # L       [dbl] [1-by-n] Vector of lower bounds (-Inf(1,n))
     # U       [dbl] [1-by-n] Vector of upper bounds (Inf(1,n))
-    # printf  [log] 1 Indicates you want output to screen (1)
+    # printf  [log] 0 No printing to screen 
+    #               1 Debugging level of output to screen (default)
+    #               2 More verbose screen output 
     # spsolver [int] Trust-region subproblem solver flag
     #
     # Optionally, a user can specify and outer-function that maps the the elements
@@ -144,7 +146,7 @@ def pounders(fun, X0, n, mpmax, nfmax, gtol, delta, nfs, m, F0, xkin, L, U, prin
         c = Fs[xkin]
         G, H = combinemodels(Cres, Gres, Hres)
         ng = np.linalg.norm(G * (np.int64(X[xkin] > L) * np.int64(G.T > 0) + np.int64(X[xkin] < U) * np.int64(G.T < 0)).T, 2)
-        if printf:
+        if printf >= 2:
             IERR = np.zeros(len(Mind))
             for i in range(len(Mind)):
                 D = X[Mind[i]] - X[xkin]
