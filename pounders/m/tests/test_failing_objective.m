8d781276469ebda7c380562f764ed44764e0e14b
Jeffrey Larson <jmlarson@anl.gov>
2022-12-12 13:07:22 -0600

    Adding a test of graceful Nan handling

---
+++ pounders/m/tests/test_failing_objective.m
@@ -0,0 +1,31 @@
+% This tests the ability of the MATLAB pounders implementation to
+% gracefully terminate when receiving a NaN as a component in the vector of
+% objective output.
+
+function [] = test_failing_objective()
+
+spsolver = 1;
+
+addpath('../');
+
+nfmax = 1000;
+gtol = 1e-13;
+n = 3;
+m = 3;
+
+xs = [10; 20; 30];
+npmax = 2 * n + 1;
+L = -Inf(1, n);
+U = Inf(1, n);
+nfs = 0;
+X0 = xs';
+F0 = [];
+xkin = 1;
+delta = 0.1;
+printf = 0;
+
+rand('seed', 1);
+objective = @(x)failing_objective(x);
+
+[X, F, flag, xk_best] = pounders(objective, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xkin, L, U, printf, spsolver);
+assert(flag == -3, "No NaN was encountered in this test, but should have been.");
