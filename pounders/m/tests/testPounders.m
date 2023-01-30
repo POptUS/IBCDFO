959704f78c99331073a6c887b2a80b306961b5d6
Jeffrey Larson <jmlarson@anl.gov>
2023-01-30 12:59:22 -0600

    Code coverage files

---
+++ pounders/m/tests/testPounders.m
@@ -0,0 +1,13 @@
+classdef test_pounders < matlab.unittest.TestCase
+    methods(Test)
+        function realSolution(testCase)
+            test_failing_objective
+        end
+        function all(testCase)
+            benchmark_pounders
+        end
+        function more(testCase)
+            test_bounds_and_sp1
+        end
+    end
+end
