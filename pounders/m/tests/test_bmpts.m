e8170a825192193db31114b4a02ebc2c08c8f944
Jeffrey Larson <jmlarson@anl.gov>
2023-02-15 15:54:06 -0600

    Trying to test all of bmpts.m

--- pounders/m/tests/test_bmpts.m
+++ pounders/m/tests/test_bmpts.m
@@ -9,22 +9,21 @@ gtol = 1e-13;
 n = 2;
 m = n;
 
-xs = 1e-8*ones(1,n);
+xs = 1e-8 * ones(1, n);
 
 npmax = 2 * n + 1;  % Maximum number of interpolation points [2*n+1]
 L = zeros(1, n); % Lower bounds
 U = ones(1, n); % Upper bounds
 
-X0 = zeros(3,n); % Starting points 
+X0 = zeros(3, n); % Starting points
 
-X0(1,:) = xs; % Near origin
-X0(2,:) = 10; % Not near origin 
-X0(3,:) = 100; % Not near origin
-
+X0(1, :) = xs; % Near origin
+X0(2, :) = 10; % Not near origin
+X0(3, :) = 100; % Not near origin
 
 objective = @(x) x; % Identity mapping
 for i = 1:3
-    F0(i,:) = objective(X0(i,:));
+    F0(i, :) = objective(X0(i, :));
 end
 
 nfs = 3; % Points that have been evaluated
