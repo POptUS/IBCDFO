0d0879da743889ade325158c697cc409c0c02e01
Stefan Wild <wild@anl.gov>
2023-02-20 09:08:07 -0800

    Addressing new error case

--- pounders/m/prepare_outputs_before_return.m
+++ pounders/m/prepare_outputs_before_return.m
@@ -12,6 +12,8 @@ function [X, F, exit_flag] = prepare_outputs_before_return(X, F, nf, exit_flag)
         disp("A NaN was encountered in an objective evaluation. Exiting.");
     elseif exit_flag == -2
         disp('Terminating because mdec == 0 with a valid model and no improvement from TRSP solution');
+    elseif exit_flag == -5
+        disp('Unable to improve model with current Pars; try dividing Par(3:4) by 10');
     elseif exit_flag == 0
         disp('g is sufficiently small');
     end
