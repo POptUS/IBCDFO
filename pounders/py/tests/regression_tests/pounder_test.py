c937f72f064f300572006a8daebf5095aa01f718
Jeffrey Larson <jmlarson@anl.gov>
2023-04-10 15:12:56 -0500

    Simplifying

--- pounders/py/tests/regression_tests/pounder_test.py
+++ pounders/py/tests/regression_tests/pounder_test.py
@@ -9,7 +9,6 @@ from general_h_funs import identity_combine as combinemodels
 from pounders import pounders
 
 # Sample calling syntax for pounders
-# func is a function imported from calFun.py as calFun
 func = lambda x: np.sum(x)
 n = 16
 
