81cb7e78170421d607574b620007a890aa6b3a08
Remi Lehe <remi.lehe@normalesup.org>
2023-04-10 15:50:17 -0700

    Add setup.py and __init__ file for pounders

---
+++ pounders/py/__init__.py
@@ -0,0 +1,1 @@
+from .pounders import *
---
+++ pounders/py/setup.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pounders',
+    version='0.1.0',
+    description='Optimization using the pounders method',
+    packages = ['pounders'],
+    package_dir = {'pounders': '.'},
+    platforms='any',
+    url='https://github.com/POptUS/IBCDFO/tree/main/pounders'
+    )
