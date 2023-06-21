fe86f1247c8c8153c2c4e2dc58ad19f2a2fdddda
Jeffrey Larson <jmlarson@anl.gov>
2023-06-21 13:17:59 -0500

    No need for BenDFO here

--- pounders/py/tests/regression_tests/calfun_wrapper.m
+++ pounders/py/tests/regression_tests/calfun_wrapper.m
@@ -2,10 +2,6 @@
 % evaluates calfun at x.
 
 function [fvec] = calfun_wrapper(x, m, nprob, probtype, fvals, nfev, np)
-
-    bendfo_root = "/home/jlarson/research/poptus/BenDFO/";
-    addpath([bendfo_root  "m/"]);
-
     BenDFO.nprob = nprob;
     BenDFO.m = m;
     BenDFO.n = length(x);
