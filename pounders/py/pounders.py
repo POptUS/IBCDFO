1641a7973616cfe819cd395792c6b61afa8c97a7
Jeffrey Larson <jmlarson@anl.gov>
2023-07-18 11:29:34 -0500

    Formatting

--- pounders/py/pounders.py
+++ pounders/py/pounders.py
@@ -129,8 +129,8 @@ def pounders(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xkin, L, U, prin
     ng = np.nan  # Needed for early termination, e.g., if a model is never built
     while nf + 1 < nfmax:
         #  1a. Compute the interpolation set.
-        D = X[:nf+1] - X[xkin] 
-        Res[:nf+1, :] = (F[:nf+1, :] - Cres)  - np.diagonal(0.5*D@(np.tensordot(D, Hres, axes = 1))).T
+        D = X[: nf + 1] - X[xkin]
+        Res[: nf + 1, :] = (F[: nf + 1, :] - Cres) - np.diagonal(0.5 * D @ (np.tensordot(D, Hres, axes=1))).T
         [Mdir, mp, valid, Gres, Hresdel, Mind] = formquad(X[0 : nf + 1, :], Res[0 : nf + 1, :], delta, xkin, npmax, Par, 0)
         if mp < n:
             [Mdir, mp] = bmpts(X[xkin], Mdir[0 : n - mp, :], L, U, delta, Par[2])
@@ -273,8 +273,8 @@ def pounders(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xkin, L, U, prin
             [Mdir, mp, valid, _, _, _] = formquad(X[: nf + 1, :], F[: nf + 1, :], delta, xkin, npmax, Par, 1)
             if not valid:  # ! One strategy for choosing model-improving point:
                 # Update model (exists because delta & xkin unchanged)
-                D = X[:nf+1] - X[xkin] 
-                Res[:nf+1, :] = (F[:nf+1, :] - Cres)  - np.diagonal(0.5*D@(np.tensordot(D, Hres, axes = 1))).T
+                D = X[: nf + 1] - X[xkin]
+                Res[: nf + 1, :] = (F[: nf + 1, :] - Cres) - np.diagonal(0.5 * D @ (np.tensordot(D, Hres, axes=1))).T
                 [_, _, valid, Gres, Hresdel, Mind] = formquad(X[: nf + 1, :], Res[: nf + 1, :], delta, xkin, npmax, Par, False)
                 Hres = Hres + Hresdel
                 # Update for modelimp; Cres unchanged b/c xkin unchanged
