8e080379e93c84e77ac5d5bf1456a9479130dfcc
Jeffrey Larson <jmlarson@anl.gov>
2023-08-24 11:52:39 -0500

    Trying to test more matlab

--- manifold_sampling/m/tests/benchmark_manifold_sampling.m
+++ manifold_sampling/m/tests/benchmark_manifold_sampling.m
@@ -2,15 +2,7 @@
 % More and Wild SIOPT paper "Benchmarking derivative-free optimization algorithms"
 function [] = benchmark_manifold_sampling()
 
-addpath('../');
-addpath('../../../../BenDFO/m/');
-addpath('../../../../BenDFO/data/');
-addpath('../h_examples/');
-addpath('../../../pounders/m'); % formquad, bmpts, boxline, phi2eval
-
-mkdir('benchmark_results');
-
-nfmax = 500;
+nfmax = 50;
 factor = 10;
 
 subprob_switch = 'linprog';
@@ -19,6 +11,10 @@ load dfo.dat;
 
 filename = ['./benchmark_results/manifold_samplingM_nfmax=' num2str(nfmax) '.mat'];
 Results = cell(1, 53);
+
+C_L1_loss = load('C_for_benchmark_probs.csv');
+D_L1_loss = load('D_for_benchmark_probs.csv');
+Qzb = load('../mpc_test_files/Q_z_and_b_for_benchmark_problems_normalized.mat')';
 
 % for row = find(cellfun(@length,Results)==0)
 for row = [1, 2, 7, 8, 43, 44, 45]
@@ -36,18 +32,19 @@ for row = [1, 2, 7, 8, 43, 44, 45]
 
     xs = dfoxs(n, nprob, factor^factor_power);
 
+    ind = find(C_L1_loss(:, 1) == mw_prob_num & C_L1_loss(:, 2) == seed);
+    C = C_L1_loss(ind, 4:m + 3);
+    D = D_L1_loss(ind, 4:m + 3);
+    Qs = Qzb.Q_mat{row, 0};
+    zs = Qzb.z_mat{row, 0};
+    cs = Qzb.b_mat{row, 0};
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Manifold sampling
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    for jj = 1:2
-        if jj == 1
-            hfun = @pw_maximum_squared;
-        end
-        if jj == 2
-            hfun = @pw_minimum_squared;
-        end
-
+    jj = 1;
+    for hfuns = {@censored_L1_loss,  @censored_L1_loss_quad_MSG,  @max_sum_beta_plus_const_viol, @piecewise_quadratic,  @pw_maximum,  @pw_maximum_squared,  @pw_minimum, @pw_minimum_squared, @quantile}
+        hfun = hfuns{1}
         Ffun = @(x)calfun_wrapper(x, BenDFO, 'smooth');
         x0 = xs';
 
@@ -58,6 +55,7 @@ for row = [1, 2, 7, 8, 43, 44, 45]
         Results{jj, row}.Fvec = F;
         Results{jj, row}.H = h;
         Results{jj, row}.X = X;
+        jj = jj + 1;
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % save(filename, 'Results');
         % save('-mat7-binary', filename, 'Results') % Octave save
