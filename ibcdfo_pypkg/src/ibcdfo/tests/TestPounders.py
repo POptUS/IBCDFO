7a42e2f8a8bfbbb8726b887e0cc0470fa4bac3bd
Jeffrey Larson <jmlarson@anl.gov>
2023-08-29 09:41:33 -0500

    Trying to get some coverage

--- ibcdfo_pypkg/src/ibcdfo/tests/TestPounders.py
+++ ibcdfo_pypkg/src/ibcdfo/tests/TestPounders.py
@@ -5,13 +5,13 @@ Unit test of compute function
 import unittest
 from pathlib import Path
 
-import ibcdfo
+import ibcdfo.pounders as pdrs
 import numpy as np
 
 _TEST_PATH = Path(__file__).resolve().parent
 
 
-class TestCompute(unittest.TestCase):
+class TestPounders(unittest.TestCase):
     # def setUp(self):
     #     fname = _TEST_PATH.joinpath("compute_test_data.csv")
     #     self.__tests = np.loadtxt(fname, delimiter=",")
@@ -19,6 +19,12 @@ class TestCompute(unittest.TestCase):
     def testSomething(self):
         self.assertTrue(True)
 
-    # def testAgain(self):
-    #     # This is just running all of the tests again...
-    #     myt.test()
+    def test_phi2eval(self):
+        D = np.eye(3)
+        T = np.zeros((3, 6))
+        T[0, 0] = 0.5
+        T[1, 3] = 0.5
+        T[2, 5] = 0.5
+
+        P = pdrs.phi2eval(D)
+        assert np.all(P == T), "Test failed"
