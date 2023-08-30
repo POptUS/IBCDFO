880a53042012deee35cf7a65d7866bc26bdd0fb3
Jeffrey Larson <jmlarson@anl.gov>
2023-08-30 09:35:51 -0500

    Debug GH actions

--- pounders/py/tests/TestPoundersSimple.py
+++ pounders/py/tests/TestPoundersSimple.py
@@ -143,4 +143,5 @@ class TestPounders(unittest.TestCase):
 
         [X, F, flag, xkin] = pdrs.pounders(func, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xind, Low, Upp, printf, spsolver, hfun, combinemodels)
 
+        print("DEBUG32:", X, X[xkin], np.linalg.norm(X[xkin] == Low), flush=True)
         assert np.all(X[xkin] == Low), "The optimum should be the lower bounds."
