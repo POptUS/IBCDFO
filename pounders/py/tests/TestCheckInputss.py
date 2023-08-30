4b6bd0496b4f651d697905fb7e9aed25662644cb
Jeffrey Larson <jmlarson@anl.gov>
2023-08-30 14:09:57 -0500

    More checking

--- pounders/py/tests/TestCheckInputss.py
+++ pounders/py/tests/TestCheckInputss.py
@@ -9,14 +9,14 @@ import numpy as np
 
 fun = np.linalg.norm
 n = 3
-X0 = 0.5 * np.ones(3)
+X0 = np.vstack((0.5 * np.ones(3), np.zeros(3)))
 npmax = 2 * n + 1
 nfmax = 10
 gtol = 1e-13
 delta = 0.1
-nfs = 1
+nfs = 2
 m = 1
-F0 = np.linalg.norm(X0)
+F0 = np.zeros((2, 1))
 xkin = 0
 L = np.zeros(n)
 U = np.ones(n)
@@ -43,6 +43,26 @@ class TestLotsOfFeatures(unittest.TestCase):
         flag = checkinputss(fun, X0, n, npmax, nfmax_to_fail, gtol, delta, nfs, m, F0, xkin, L, U)[0]
         assert flag == -1
 
+    def test_checkinputts5(self):
+        gtol_to_fail = 0
+        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol_to_fail, delta, nfs, m, F0, xkin, L, U)[0]
+        assert flag == -1
+
+    def test_checkinputts6(self):
+        delta_to_fail = 0
+        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta_to_fail, nfs, m, F0, xkin, L, U)[0]
+        assert flag == -1
+
+    def test_checkinputts7(self):
+        F0_to_fail = np.zeros((2, 7))
+        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0_to_fail, xkin, L, U)[0]
+        assert flag == -1
+
+    # def test_checkinputts8(self):
+    #     F0_to_fail = np.zeros((7,1))
+    #     flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0_to_fail, xkin, L, U)[0]
+    #     assert flag == -1
+
     # def test_checkinputts5(self):
     #     F0_to_fail = np.zeros(2 * m)
     #     flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0_to_fail, xkin, L, U)[0]
@@ -54,4 +74,7 @@ if __name__ == "__main__":  # pragma: no cover
     TestLotsOfFeatures.test_checkinputts2([])
     TestLotsOfFeatures.test_checkinputts3([])
     TestLotsOfFeatures.test_checkinputts4([])
-    # TestLotsOfFeatures.test_checkinputts5([])
+    TestLotsOfFeatures.test_checkinputts5([])
+    TestLotsOfFeatures.test_checkinputts6([])
+    TestLotsOfFeatures.test_checkinputts7([])
+    # TestLotsOfFeatures.test_checkinputts8([])
