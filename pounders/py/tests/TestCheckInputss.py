ce552631fa5756df7397d88c5e01b150c85547f5
Jeffrey Larson <jmlarson@anl.gov>
2023-08-31 08:52:46 -0500

    Using setup for checkinputss test

--- pounders/py/tests/TestCheckInputss.py
+++ pounders/py/tests/TestCheckInputss.py
@@ -31,72 +31,94 @@ class TestLotsOfFeatures(unittest.TestCase):
 
     def test_checkinputts2(self):
         n_to_fail = 2
-        flag = checkinputss(fun, X0, n_to_fail, npmax, nfmax, gtol, delta, nfs, m, F0, xkin, L, U)[0]
+        flag = checkinputss(self.fun, self.X0, n_to_fail, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)[0]
         assert flag == -1
 
     def test_checkinputts3(self):
         npmax_to_warn = 1
-        flag = checkinputss(fun, X0, n, npmax_to_warn, nfmax, gtol, delta, nfs, m, F0, xkin, L, U)[0]
+        flag = checkinputss(self.fun, self.X0, self.n, npmax_to_warn, self.nfmax, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)[0]
         assert flag == 0
 
     def test_checkinputts4(self):
         nfmax_to_fail = 0
-        flag = checkinputss(fun, X0, n, npmax, nfmax_to_fail, gtol, delta, nfs, m, F0, xkin, L, U)[0]
+        flag = checkinputss(self.fun, self.X0, self.n, self.npmax, nfmax_to_fail, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)[0]
         assert flag == -1
 
     def test_checkinputts5(self):
         gtol_to_fail = 0
-        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol_to_fail, delta, nfs, m, F0, xkin, L, U)[0]
+        flag = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax,
+                gtol_to_fail, self.delta, self.nfs, self.m, self.F0,
+                self.xkin, self.L, self.U)[0]
         assert flag == -1
 
     def test_checkinputts6(self):
         delta_to_fail = 0
-        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta_to_fail, nfs, m, F0, xkin, L, U)[0]
+        flag = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax,
+                self.gtol, delta_to_fail, self.nfs, self.m, self.F0,
+                self.xkin, self.L, self.U)[0]
         assert flag == -1
 
     def test_checkinputts7(self):
-        F0_to_fail = np.zeros((2, 7))
-        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0_to_fail, xkin, L, U)[0]
+        F0_to_fail = np.zeros((self.nfs, 3*self.nfs))
+        flag = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax,
+                self.gtol, self.delta, self.nfs, self.m, F0_to_fail,
+                self.xkin, self.L, self.U)[0]
         assert flag == -1
 
     def test_checkinputts8(self):
-        F0_to_warn = np.zeros((7, 1))
-        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0_to_warn, xkin, L, U)[0]
+        F0_to_warn = np.zeros((3*self.nfs, 1))
+        flag = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax,
+                self.gtol, self.delta, self.nfs, self.m, F0_to_warn,
+                self.xkin, self.L, self.U)[0]
         assert flag == 0
 
     def test_checkinputts9(self):
-        F0_to_fail = np.nan * F0
-        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0_to_fail, xkin, L, U)[0]
+        F0_to_fail = np.nan * self.F0
+        flag = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax,
+                self.gtol, self.delta, self.nfs, self.m, F0_to_fail,
+                self.xkin, self.L, self.U)[0]
         assert flag == -1
 
     def test_checkinputts10(self):
         xkin_to_fail = -1
-        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xkin_to_fail, L, U)[0]
+        flag = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax,
+                self.gtol, self.delta, self.nfs, self.m, self.F0,
+                xkin_to_fail, self.L, self.U)[0]
         assert flag == -1
 
     def test_checkinputts11(self):
-        L_to_fail = np.hstack((L, L))
-        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xkin, L_to_fail, U)[0]
+        L_to_fail = np.hstack((self.L, self.L))
+        flag = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax,
+                self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin,
+                L_to_fail, self.U)[0]
         assert flag == -1
 
     def test_checkinputts12(self):
-        L_to_warn = np.atleast_2d(L).T
-        U_to_warn = np.atleast_2d(U).T
-        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xkin, L_to_warn, U_to_warn)[0]
+        L_to_warn = np.atleast_2d(self.L).T
+        U_to_warn = np.atleast_2d(self.U).T
+        flag = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax,
+                self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin,
+                L_to_warn, U_to_warn)[0]
         assert flag == 0
 
     def test_checkinputts13(self):
-        L_to_fail = np.zeros((2, n))
-        U_to_fail = np.zeros((2, n))
-        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xkin, L_to_fail, U_to_fail)[0]
+        L_to_fail = np.zeros((2, self.n))
+        U_to_fail = np.zeros((2, self.n))
+        flag = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax,
+                self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin,
+                L_to_fail, U_to_fail)[0]
         assert flag == -1
 
     def test_checkinputts14(self):
-        L_to_error = U
-        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xkin, L_to_error, U)[0]
+        L_to_error = self.U
+        flag = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax,
+                self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin,
+                L_to_error, self.U)[0]
         assert flag == -1
 
     def test_checkinputts15(self):
-        L_to_error = 0.9 * U
-        flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xkin, L_to_error, U)[0]
+        L_to_error = 0.9 * self.U
+        flag = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax,
+                self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin,
+                L_to_error, self.U)[0]
         assert flag == -1
