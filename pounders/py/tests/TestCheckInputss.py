ae6cd8d99c7f457bbe52ba2e149a296eb00ff972
Jeffrey Larson <jmlarson@anl.gov>
2023-08-31 08:44:48 -0500

    Some changes after talking to Jared

--- pounders/py/tests/TestCheckInputss.py
+++ pounders/py/tests/TestCheckInputss.py
@@ -7,25 +7,26 @@ import unittest
 import numpy as np
 from ibcdfo.pounders.checkinputss import checkinputss
 
-fun = np.linalg.norm
-n = 3
-X0 = np.vstack((0.5 * np.ones(3), np.zeros(3)))
-npmax = 2 * n + 1
-nfmax = 10
-gtol = 1e-13
-delta = 0.1
-nfs = 2
-m = 1
-F0 = np.zeros((2, 1))
-xkin = 0
-L = np.zeros(n)
-U = np.ones(n)
-
 
 class TestLotsOfFeatures(unittest.TestCase):
+    def setUp(self):
+        self.fun = np.linalg.norm
+        self.n = 3
+        self.X0 = np.vstack((0.5 * np.ones(self.n), np.zeros(self.n)))
+        self.npmax = 2 * self.n + 1
+        self.nfmax = 10
+        self.gtol = 1e-13
+        self.delta = 0.1
+        self.nfs = 2
+        self.m = 1
+        self.F0 = np.zeros((self.nfs, 1))
+        self.xkin = 0
+        self.L = np.zeros(self.n)
+        self.U = np.ones(self.n)
+
     def test_checkinputts1(self):
         fun_to_fail = []
-        flag = checkinputss(fun_to_fail, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xkin, L, U)[0]
+        flag = checkinputss(fun_to_fail, self.X0, self.n, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)[0]
         assert flag == -1
 
     def test_checkinputts2(self):
@@ -99,26 +100,3 @@ class TestLotsOfFeatures(unittest.TestCase):
         L_to_error = 0.9 * U
         flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xkin, L_to_error, U)[0]
         assert flag == -1
-
-    # def test_checkinputts5(self):
-    #     F0_to_fail = np.zeros(2 * m)
-    #     flag = checkinputss(fun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0_to_fail, xkin, L, U)[0]
-    #     assert flag == -1
-
-
-if __name__ == "__main__":  # pragma: no cover
-    TestLotsOfFeatures.test_checkinputts1([])
-    TestLotsOfFeatures.test_checkinputts2([])
-    TestLotsOfFeatures.test_checkinputts3([])
-    TestLotsOfFeatures.test_checkinputts4([])
-    TestLotsOfFeatures.test_checkinputts5([])
-    TestLotsOfFeatures.test_checkinputts6([])
-    TestLotsOfFeatures.test_checkinputts7([])
-    TestLotsOfFeatures.test_checkinputts8([])
-    TestLotsOfFeatures.test_checkinputts9([])
-    TestLotsOfFeatures.test_checkinputts10([])
-    TestLotsOfFeatures.test_checkinputts11([])
-    TestLotsOfFeatures.test_checkinputts12([])
-    TestLotsOfFeatures.test_checkinputts13([])
-    TestLotsOfFeatures.test_checkinputts14([])
-    TestLotsOfFeatures.test_checkinputts15([])
