13b2e7da5fde1941c9db69f0ccfb597fe5ef0eff
Jeffrey Larson <jmlarson@anl.gov>
2023-09-11 18:27:56 -0500

    Double underscores

--- pounders/py/tests/TestCheckInputss.py
+++ pounders/py/tests/TestCheckInputss.py
@@ -24,7 +24,7 @@ class TestLotsOfFeatures(unittest.TestCase):
         self.L = np.zeros(self.n)
         self.U = np.ones(self.n)
 
-    def _testCommonFinalConditions(self, out, flag):
+    def __testCommonFinalConditions(self, out, flag):
         if flag == "success":
             self.assertEqual(out[0], 1, "Should not have failed")
         elif flag == "fail":
@@ -36,81 +36,81 @@ class TestLotsOfFeatures(unittest.TestCase):
 
     def test_checkinputts0(self):
         out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
-        self._testCommonFinalConditions(out, "success")
+        self.__testCommonFinalConditions(out, "success")
 
     def test_checkinputts1(self):
         fun_to_fail = []
         out = checkinputss(fun_to_fail, self.X0, self.n, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
-        self._testCommonFinalConditions(out, "fail")
+        self.__testCommonFinalConditions(out, "fail")
 
     def test_checkinputts2(self):
         n_to_fail = 2
         out = checkinputss(self.fun, self.X0, n_to_fail, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
-        self._testCommonFinalConditions(out, "fail")
+        self.__testCommonFinalConditions(out, "fail")
 
     def test_checkinputts3(self):
         npmax_to_warn = 1
         out = checkinputss(self.fun, self.X0, self.n, npmax_to_warn, self.nfmax, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
-        self._testCommonFinalConditions(out, "warn")
+        self.__testCommonFinalConditions(out, "warn")
 
     def test_checkinputts4(self):
         nfmax_to_fail = 0
         out = checkinputss(self.fun, self.X0, self.n, self.npmax, nfmax_to_fail, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
-        self._testCommonFinalConditions(out, "fail")
+        self.__testCommonFinalConditions(out, "fail")
 
     def test_checkinputts5(self):
         gtol_to_fail = 0
         out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, gtol_to_fail, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
-        self._testCommonFinalConditions(out, "fail")
+        self.__testCommonFinalConditions(out, "fail")
 
     def test_checkinputts6(self):
         delta_to_fail = 0
         out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.gtol, delta_to_fail, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
-        self._testCommonFinalConditions(out, "fail")
+        self.__testCommonFinalConditions(out, "fail")
 
     def test_checkinputts7(self):
         F0_to_fail = np.zeros((self.nfs, 3 * self.nfs))
         out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, F0_to_fail, self.xkin, self.L, self.U)
-        self._testCommonFinalConditions(out, "fail")
+        self.__testCommonFinalConditions(out, "fail")
 
     def test_checkinputts8(self):
         F0_to_warn = np.zeros((3 * self.nfs, 1))
         out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, F0_to_warn, self.xkin, self.L, self.U)
-        self._testCommonFinalConditions(out, "warn")
+        self.__testCommonFinalConditions(out, "warn")
 
     def test_checkinputts9(self):
         F0_to_fail = np.nan * self.F0
         out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, F0_to_fail, self.xkin, self.L, self.U)
-        self._testCommonFinalConditions(out, "fail")
+        self.__testCommonFinalConditions(out, "fail")
 
     def test_checkinputts10(self):
         xkin_to_fail = -1
         out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, self.F0, xkin_to_fail, self.L, self.U)
-        self._testCommonFinalConditions(out, "fail")
+        self.__testCommonFinalConditions(out, "fail")
 
     def test_checkinputts11(self):
         L_to_fail = np.hstack((self.L, self.L))
         out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, L_to_fail, self.U)
-        self._testCommonFinalConditions(out, "fail")
+        self.__testCommonFinalConditions(out, "fail")
 
     def test_checkinputts12(self):
         L_to_warn = np.atleast_2d(self.L).T
         U_to_warn = np.atleast_2d(self.U).T
         out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, L_to_warn, U_to_warn)
-        self._testCommonFinalConditions(out, "warn")
+        self.__testCommonFinalConditions(out, "warn")
 
     def test_checkinputts13(self):
         L_to_fail = np.zeros((2, self.n))
         U_to_fail = np.zeros((2, self.n))
         out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, L_to_fail, U_to_fail)
-        self._testCommonFinalConditions(out, "fail")
+        self.__testCommonFinalConditions(out, "fail")
 
     def test_checkinputts14(self):
         L_to_error = self.U
         out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, L_to_error, self.U)
-        self._testCommonFinalConditions(out, "fail")
+        self.__testCommonFinalConditions(out, "fail")
 
     def test_checkinputts15(self):
         L_to_error = 0.9 * self.U
         out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.gtol, self.delta, self.nfs, self.m, self.F0, self.xkin, L_to_error, self.U)
-        self._testCommonFinalConditions(out, "fail")
+        self.__testCommonFinalConditions(out, "fail")
