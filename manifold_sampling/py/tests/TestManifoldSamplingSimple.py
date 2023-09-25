7807aa2a5d3e6f57f6f73418bce3cf30d377a6fe
Jeffrey Larson <jmlarson@anl.gov>
2023-09-25 12:31:50 -0500

    format

--- manifold_sampling/py/tests/TestManifoldSamplingSimple.py
+++ manifold_sampling/py/tests/TestManifoldSamplingSimple.py
@@ -19,7 +19,7 @@ class TestManifoldSampling(unittest.TestCase):
 
             return fvec
 
-        subprob_switch = "linprog" 
+        subprob_switch = "linprog"
         nfmax = 1000
         X0 = np.array([10, 20, 30])
         L = -np.inf * np.ones(3)
@@ -34,6 +34,6 @@ class TestManifoldSampling(unittest.TestCase):
         else:
             assert 0, "Didn't fail like it should have"
 
-        L = np.append(L,L)
+        L = np.append(L, L)
         [X, F, h, xk_best, flag] = msp.manifold_sampling_primal(pw_maximum, failing_objective, X0, L, U, nfmax, subprob_switch)
         self.assertEqual(flag, -1, "We are testing proper failure of pounders")
