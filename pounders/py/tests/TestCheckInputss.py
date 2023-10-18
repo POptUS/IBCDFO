9af364ef8ad84cac2c3cfc7f83ab07871e532508
Jeffrey Larson <jmlarson@anl.gov>
2023-10-18 14:49:06 -0500

    Restoring files

---
+++ pounders/py/tests/TestCheckInputss.py
@@ -0,0 +1,116 @@
+"""
+Unit test of compute function
+"""
+
+import unittest
+
+import numpy as np
+from ibcdfo.pounders.checkinputss import checkinputss
+
+
+class TestLotsOfFeatures(unittest.TestCase):
+    def setUp(self):
+        self.fun = np.linalg.norm
+        self.n = 3
+        self.X0 = np.vstack((0.5 * np.ones(self.n), np.zeros(self.n)))
+        self.npmax = 2 * self.n + 1
+        self.nfmax = 10
+        self.g_tol = 1e-13
+        self.delta = 0.1
+        self.nfs = 2
+        self.m = 1
+        self.F0 = np.zeros((self.nfs, 1))
+        self.xkin = 0
+        self.L = np.zeros(self.n)
+        self.U = np.ones(self.n)
+
+    def __testCommonFinalConditions(self, out, flag):
+        if flag == "success":
+            self.assertEqual(out[0], 1, "Should not have failed")
+        elif flag == "fail":
+            self.assertEqual(out[0], -1, "Should have failed")
+        elif flag == "warn":
+            self.assertEqual(out[0], 0, "Should have warned, but not failed")
+
+        self.assertEqual(len(out), 7, "Should always have 7 outputs from checkinputss")
+
+    def test_checkinputts0(self):
+        out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.g_tol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
+        self.__testCommonFinalConditions(out, "success")
+
+    def test_checkinputts1(self):
+        fun_to_fail = []
+        out = checkinputss(fun_to_fail, self.X0, self.n, self.npmax, self.nfmax, self.g_tol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
+        self.__testCommonFinalConditions(out, "fail")
+
+    def test_checkinputts2(self):
+        n_to_fail = 2
+        out = checkinputss(self.fun, self.X0, n_to_fail, self.npmax, self.nfmax, self.g_tol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
+        self.__testCommonFinalConditions(out, "fail")
+
+    def test_checkinputts3(self):
+        npmax_to_warn = 1
+        out = checkinputss(self.fun, self.X0, self.n, npmax_to_warn, self.nfmax, self.g_tol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
+        self.__testCommonFinalConditions(out, "warn")
+
+    def test_checkinputts4(self):
+        nfmax_to_fail = 0
+        out = checkinputss(self.fun, self.X0, self.n, self.npmax, nfmax_to_fail, self.g_tol, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
+        self.__testCommonFinalConditions(out, "fail")
+
+    def test_checkinputts5(self):
+        g_tol_to_fail = 0
+        out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, g_tol_to_fail, self.delta, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
+        self.__testCommonFinalConditions(out, "fail")
+
+    def test_checkinputts6(self):
+        delta_to_fail = 0
+        out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.g_tol, delta_to_fail, self.nfs, self.m, self.F0, self.xkin, self.L, self.U)
+        self.__testCommonFinalConditions(out, "fail")
+
+    def test_checkinputts7(self):
+        F0_to_fail = np.zeros((self.nfs, 3 * self.nfs))
+        out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.g_tol, self.delta, self.nfs, self.m, F0_to_fail, self.xkin, self.L, self.U)
+        self.__testCommonFinalConditions(out, "fail")
+
+    def test_checkinputts8(self):
+        F0_to_warn = np.zeros((3 * self.nfs, 1))
+        out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.g_tol, self.delta, self.nfs, self.m, F0_to_warn, self.xkin, self.L, self.U)
+        self.__testCommonFinalConditions(out, "warn")
+
+    def test_checkinputts9(self):
+        F0_to_fail = np.nan * self.F0
+        out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.g_tol, self.delta, self.nfs, self.m, F0_to_fail, self.xkin, self.L, self.U)
+        self.__testCommonFinalConditions(out, "fail")
+
+    def test_checkinputts10(self):
+        xkin_to_fail = -1
+        out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.g_tol, self.delta, self.nfs, self.m, self.F0, xkin_to_fail, self.L, self.U)
+        self.__testCommonFinalConditions(out, "fail")
+
+    def test_checkinputts11(self):
+        L_to_fail = np.hstack((self.L, self.L))
+        out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.g_tol, self.delta, self.nfs, self.m, self.F0, self.xkin, L_to_fail, self.U)
+        self.__testCommonFinalConditions(out, "fail")
+
+    def test_checkinputts12(self):
+        L_to_warn = np.atleast_2d(self.L).T
+        U_to_warn = np.atleast_2d(self.U).T
+        out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.g_tol, self.delta, self.nfs, self.m, self.F0, self.xkin, L_to_warn, U_to_warn)
+        self.__testCommonFinalConditions(out, "warn")
+
+    def test_checkinputts13(self):
+        L_to_fail = np.zeros((2, self.n))
+        U_to_fail = np.zeros((2, self.n))
+        out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.g_tol, self.delta, self.nfs, self.m, self.F0, self.xkin, L_to_fail, U_to_fail)
+        self.__testCommonFinalConditions(out, "fail")
+
+    def test_checkinputts14(self):
+        L_to_error = self.U
+        out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.g_tol, self.delta, self.nfs, self.m, self.F0, self.xkin, L_to_error, self.U)
+        self.__testCommonFinalConditions(out, "fail")
+
+    def test_checkinputts15(self):
+        L_to_error = 0.9 * self.U
+        out = checkinputss(self.fun, self.X0, self.n, self.npmax, self.nfmax, self.g_tol, self.delta, self.nfs, self.m, self.F0, self.xkin, L_to_error, self.U)
+        self.__testCommonFinalConditions(out, "fail")
---
+++ pounders/py/tests/TestPoundersSimple.py
@@ -0,0 +1,153 @@
+"""
+Unit test of compute function
+"""
+
+import unittest
+
+import ibcdfo.pounders as pdrs
+import numpy as np
+
+
+class TestPounders(unittest.TestCase):
+    def test_phi2eval(self):
+        D = np.eye(3)
+        T = np.zeros((3, 6))
+        T[0, 0] = 0.5
+        T[1, 3] = 0.5
+        T[2, 5] = 0.5
+
+        P = pdrs.phi2eval(D)
+        self.assertTrue(np.all(P == T), "Test failed")
+
+    def test_failing_objective(self):
+        def failing_objective(x):
+            fvec = x
+
+            if np.random.uniform() < 0.1:
+                fvec[0] = np.nan
+
+            return fvec
+
+        spsolver = 1
+        nfmax = 1000
+        g_tol = 1e-13
+        n = 3
+        m = 3
+
+        X0 = np.array([10, 20, 30])
+        L = -np.inf * np.ones(n)
+        U = np.inf * np.ones(n)
+        # nfs = 0
+        # F0 = []
+        # xkin = 0
+        delta = 0.1
+        printf = 0
+
+        np.random.seed(1)
+
+        Opts = {"spsolver": spsolver, "printf": printf}
+        [X, F, flag, xk_best] = pdrs.pounders(failing_objective, X0, n, nfmax, g_tol, delta, m, L, U, Options=Opts)
+        self.assertEqual(flag, -3, "No NaN was encountered in this test, but should have been.")
+
+        F0 = np.array([1.0, 2.0])
+        Prior = {"X_init": X0, "F_init": F0, "nfs": 2, "xk_init": 0}
+        Opts = {"spsolver": spsolver, "printf": printf}
+        [X, F, flag, xk_best] = pdrs.pounders(failing_objective, X0, n, nfmax, g_tol, delta, m, L, U, Prior=Prior, Options=Opts)
+        self.assertEqual(flag, -1, "We are testing proper failure of pounders")
+
+    def test_basic_pounders_usage(self):
+        def vecFun(x):
+            """
+            Input:
+                x is a numpy array (column / row vector)
+            Output:
+                x + x^2 as a row vector
+            """
+            if np.shape(x)[0] > 1:
+                x = np.reshape(x, (1, max(np.shape(x))))
+            return x + (x**2)
+
+        # Sample calling syntax for pounders
+        # func is a function imported from calFun.py as calFun
+        func = vecFun
+        # n [int] Dimension (number of continuous variables)
+        n = 2
+        # X0 [dbl] [min(fstart,1)-by-n] Set of initial points  (zeros(1,n))
+        X0 = np.zeros((10, 2))
+        X0[0, :] = 0.5 * np.ones((1, 2))
+        # nfmax [int] Maximum number of function evaluations (>n+1) (100)
+        nfmax = 60
+        # g_tol [dbl] Tolerance for the 2-norm of the model gradient (1e-4)
+        g_tol = 10**-13
+        # delta [dbl] Positive trust region radius (.1)
+        delta = 0.1
+        # nfs [int] Number of function values (at X0) known in advance (0)
+        nfs = 10
+        # m [int] number of residuals
+        m = 2
+        # F0 [dbl] [fstart-by-1] Set of known function values  ([])
+        F0 = np.zeros((10, 2))
+        # xind [int] Index of point in X0 at which to start from (1)
+        xind = 0
+        # Low [dbl] [1-by-n] Vector of lower bounds (-Inf(1,n))
+        Low = np.zeros((1, n))
+        # Upp [dbl] [1-by-n] Vector of upper bounds (Inf(1,n))
+        Upp = np.ones((1, n))
+        spsolver = 2
+
+        np.random.seed(1)
+        F0[0, :] = func(X0[0, :])
+        for i in range(1, 10):
+            X0[i, :] = X0[0, :] + 0.2 * np.random.rand(1, 2) - 0.1
+            F0[i, :] = func(X0[i, :])
+
+        Prior = {"X_init": X0, "F_init": F0, "nfs": nfs, "xk_init": xind}
+        Opts = {"spsolver": spsolver, "printf": True}
+        [X, F, flag, xkin] = pdrs.pounders(func, X0, n, nfmax, g_tol, delta, m, Low, Upp, Options=Opts, Model={"npmax": int(0.5 * (n + 1) * (n + 2))}, Prior=Prior)
+
+    def test_pounders_one_output(self):
+        combinemodels = pdrs.identity_combine
+
+        # Sample calling syntax for pounders
+        func = lambda x: np.sum(x)
+        n = 16
+
+        X0 = np.ones((n, 1))  # Test giving of column vector
+        nfmax = 200
+        g_tol = 10**-13
+        delta = 0.1
+        nfs = 1
+        m = 1
+        F0 = func(X0)
+        xind = 0
+        Low = -0.1 * np.arange(n)
+        Upp = np.inf * np.ones(n)
+
+        hfun = lambda F: F
+        Opts = {"spsolver": 1, "hfun": hfun, "combinemodels": combinemodels}
+        Prior = {"X_init": X0, "F_init": F0, "nfs": nfs, "xk_init": xind}
+        [X, F, flag, xkin] = pdrs.pounders(func, X0, n, nfmax, g_tol, delta, m, Low, Upp, Options=Opts, Model={}, Prior=Prior)
+
+        self.assertTrue(np.linalg.norm(X[xkin] - Low) <= 1e-8, "The optimum should be the lower bounds.")
+
+    def test_pounders_maximizing_sum_squares(self):
+        combinemodels = pdrs.neg_leastsquares
+
+        # Sample calling syntax for pounders
+        func = lambda x: x
+        n = 16
+
+        X0 = 0.4 * np.ones(n)
+        nfmax = 200
+        g_tol = 10**-13
+        delta = 0.1
+        m = n
+        Low = 0.1 * np.ones(n)
+        Upp = np.ones(n)
+
+        hfun = lambda F: -1.0 * np.sum(F**2)
+
+        Opts = {"spsolver": 1, "hfun": hfun, "combinemodels": combinemodels, "printf": 2}
+        [X, F, flag, xkin] = pdrs.pounders(func, X0, n, nfmax, g_tol, delta, m, Low, Upp, Options=Opts, Model={})
+
+        self.assertTrue(np.linalg.norm(X[xkin] - Upp) <= 1e-8, "The optimum should be the upper bounds.")
