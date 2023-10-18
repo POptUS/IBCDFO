31d0a0d0eddbe8cb397d3804a3c690ac38bce36e
Jeffrey Larson <jmlarson@anl.gov>
2023-10-18 15:39:59 -0500

    Trying to debug github actions

--- pounders/py/pounders.py
+++ pounders/py/pounders.py
@@ -111,8 +111,12 @@ def pounders(fun, X_0, n, nf_max, g_tol, delta_0, m, L, U, Prior=None, Options=N
     else:
         key_list = ["nfs", "X_init", "F_init", "xk_init"]
         assert set(Prior.keys()) == set(key_list), "Prior keys must be {key_list}"
-        assert all(np.atleast_2d(Prior["X_init"])[Prior["xk_init"]] == X_0), "Starting point X_0 doesn't match row in Prior['X_init']"
         assert (np.atleast_2d(Prior["X_init"]).shape[0] == Prior["nfs"]) and (np.atleast_2d(Prior["F_init"]).shape[0] == Prior["nfs"]), "Prior X_init and F_init must have nfs rows"
+        if not all(np.atleast_2d(Prior["X_init"])[Prior["xk_init"]] == X_0):
+            print("jeff", np.atleast_2d(Prior["X_init"])[Prior["xk_init"]])
+            print("jeff", X_0)
+
+        assert all(np.atleast_2d(Prior["X_init"])[Prior["xk_init"]] == X_0), "Starting point X_0 doesn't match row in Prior['X_init']"
 
     if Options is None:
         Options = {}
