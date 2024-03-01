cdc556e67163fca61f3974e73f0a47e5f7cc5702
Jeffrey Larson <jmlarson@anl.gov>
2024-03-01 12:28:39 -0600

    formatting

--- pounders/m/general_h_funs/identity_combine.m
+++ pounders/m/general_h_funs/identity_combine.m
@@ -1,3 +1,4 @@
 function [G, H] = identity_combine(Cres, Gres, Hres)
-    G = Gres; H = Hres; 
+    G = Gres;
+    H = Hres;
 end
--- pounders/m/tests/test_one_m_pounders.m
+++ pounders/m/tests/test_one_m_pounders.m
@@ -1,10 +1,10 @@
 % This tests the ability of the MATLAB pounders to solve a one-residual
 % problem. The benefit of pounders comes from its handling of the separate
-% residuals, and combinging them using the known form of an outer function. 
+% residuals, and combinging them using the known form of an outer function.
 % So this is not how pounders is intended to be used, but can be a comparison
 % method.
 
-Ffun = @(x) ((1-x(1)).^2)+(100*((x(2)-(x(1)^2)).^2)); % Rosenbrock
+Ffun = @(x) ((1 - x(1)).^2) + (100 * ((x(2) - (x(1)^2)).^2)); % Rosenbrock
 X0 = [-1.2, 1];
 
 n = 2;
@@ -25,5 +25,5 @@ combinemodels = @identity_combine;
 
 [X, F, hf, flag, xkin] = ...
     pounders(Ffun, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xkin, L, U, printf, spsolver, hfun, combinemodels);
-assert(flag == 0, "We should solve this within 200 evaluations")
-assert(norm(X(xkin,:) - ones(1,2)) <= gtol*10, "We should be within 10*gtol of the known optimum [1,1]")
+assert(flag == 0, "We should solve this within 200 evaluations");
+assert(norm(X(xkin, :) - ones(1, 2)) <= gtol * 10, "We should be within 10*gtol of the known optimum [1,1]");
