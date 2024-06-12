8016a0e34685c4bbf8df4c64212a1cdaa6357a6c
Jeffrey Larson <jmlarson@anl.gov>
2024-06-12 14:02:12 -0500

    Showing a failure on main

--- pounders/py/tests/test_for_matt.py
+++ pounders/py/tests/test_for_matt.py
@@ -2,7 +2,8 @@
 Unit test of compute function
 """
 
-import unittest
+from dfoxs import dfoxs
+from calfun import calfun
 
 import ibcdfo.pounders as pdrs
 import numpy as np
@@ -10,11 +11,9 @@ import numpy as np
 
 combinemodels = pdrs.identity_combine
 
-# Sample calling syntax for pounders
-func = lambda x: np.sum(x)
-n = 16
-
 dfo = np.loadtxt("dfo.dat")
+
+factor = 10
 
 for row, (nprob, n, m, factor_power) in enumerate(dfo[10:11]):
     n = int(n)
@@ -25,23 +24,22 @@ for row, (nprob, n, m, factor_power) in enumerate(dfo[10:11]):
         # assert len(out) == m, "Incorrect output dimension"
         return np.squeeze(out)
 
+    X0 = dfoxs(n, nprob, int(factor**factor_power))
+    print(X0)
     Low = -np.inf * np.ones((1, n))  # 1-by-n Vector of lower bounds [zeros(1, n)]
     Upp = np.inf * np.ones((1, n))  # 1-by-n Vector of upper bounds [ones(1, n)]
 
-    X0 = np.ones((n, 1))  # Test giving of column vector
     npmax = 2 * n + 1
     nfmax = 500
     gtol = 10**-13
     delta = 0.1
     nfs = 1
-    m = 1
-    F0 = func(X0)
+    F0 = Ffun(X0)
     xind = 0
-    Low = -0.1 * np.arange(n)
-    Upp = np.inf * np.ones(n)
+
     printf = False
     spsolver = 1
 
     hfun = lambda F: np.squeeze(F)
 
-    [X, F, flag, xkin] = pdrs.pounders(func, X0, n, npmax, nfmax, gtol, delta, nfs, m, F0, xind, Low, Upp, printf, spsolver, hfun, combinemodels)
+    [X, F, flag, xkin] = pdrs.pounders(Ffun, X0, n, npmax, nfmax, gtol, delta, nfs, 1, F0, xind, Low, Upp, printf, spsolver, hfun, combinemodels)
