47b844c1b5a36650b420a1e2564367cedef52f85
Jared O'Neal <joneal@anl.gov>
2024-08-05 10:14:30 -0500

    Add in Python test script to match new MATLAB script.

---
+++ tools/test_ibcdfo_installation.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+"""
+Run the script with -h to obtain more information regarding the script.
+"""
+
+import sys
+import inspect
+import argparse
+import importlib
+
+from pathlib import Path
+
+# ----- HARDCODED VALUES
+# Exit codes so that this can be used in CI build server
+_FAILURE = 1
+_SUCCESS = 0
+
+
+try:
+    import ibcdfo
+except ImportError as error:
+    print()
+    print(f"ERROR: {error.name} Python package not installed")
+    print()
+    exit(_FAILURE)
+
+
+def main():
+    # Keep output compact so that package info is seen with each run
+    DEFAULT_VERBOSITY = 1
+    VALID_VERBOSITY = [0, 1, 2]
+
+    # ----- SPECIFY COMMAND LINE USAGE
+    DESCRIPTION = "Return status of ibcdfo Python package full testing " + \
+                  "as exit code for use with CI\n"
+    VERBOSE_HELP = "Verbosity level of Python unittest logging"
+    parser = argparse.ArgumentParser(
+        description=DESCRIPTION,
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument("--verbose", "-v", type=int,
+                        choices=VALID_VERBOSITY, default=DEFAULT_VERBOSITY,
+                        help=VERBOSE_HELP)
+
+    # ----- GET COMMAND LINE ARGUMENTS
+    args = parser.parse_args()
+    verbosity_level = args.verbose
+
+    # ----- PRINT VERSION INFORMATION
+    pkg = importlib.metadata.distribution("ibcdfo")
+    location = Path(inspect.getfile(ibcdfo)).parents[0]
+
+    print()
+    print("Name: {}".format(pkg.metadata["Name"]))
+    print("Version: {}".format(pkg.metadata["Version"]))
+    print("Summary: {}".format(pkg.metadata["Summary"]))
+    print("Homepage: {}".format(pkg.metadata["Home-page"]))
+    print("License: {}".format(pkg.metadata["License"]))
+    print("Python requirements: {}".format(pkg.metadata["Requires-Python"]))
+    print("Package dependencies:")
+    for dependence in pkg.metadata.get_all("Requires-Dist"):
+        print(f"\t{dependence}")
+    print("Location: {}".format(location))
+    print()
+    sys.stdout.flush()
+
+    # ----- RUN FULL TEST SUITE
+    return _SUCCESS if ibcdfo.test(verbosity_level) else _FAILURE
+
+
+if __name__ == "__main__":
+    exit(main())
