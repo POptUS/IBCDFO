7d7822362dbc6c5fd613e4b8942100955032ed6c
Jeffrey Larson <jmlarson@anl.gov>
2024-08-19 14:22:23 -0500

    mh

--- manifold_sampling/m/product_of_cells.m
+++ manifold_sampling/m/product_of_cells.m
@@ -8,7 +8,7 @@ function output = product_of_cells(varargin)
     input = varargin(1:nargin);
     L = length(input);
     assert(all(cellfun(@iscell, input)), "We need everything to be a cell");
-    
+
     % ndgrid generates L grids of indices, one for each input
     [ix{1:L}] = ndgrid(input{:});
 
