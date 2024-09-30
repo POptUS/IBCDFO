384b8eedc3dca4cc6d90eb39d4f21ec4c035b65a
Jeffrey Larson <jmlarson@anl.gov>
2024-09-30 10:15:05 -0500

    Terminating when delta_min is encountered

--- pounders/py/prepare_outputs_before_return.py
+++ pounders/py/prepare_outputs_before_return.py
@@ -12,12 +12,14 @@ def prepare_outputs_before_return(X, F, hF, nf, exit_flag):
     elif exit_flag == -3:
         print("A NaN was encountered in an objective evaluation. Exiting.")
     elif exit_flag == -2:
-        print("Terminating because mdec == 0 with a valid model and no improvement from TRSP solution")
+        print("Terminating because mdec == 0 with a valid model and no improvement from TRSP solution.")
     elif exit_flag == -5:
-        print("Unable to improve model with current Pars; try dividing Par[2:3] by 10")
+        print("Unable to improve model with current Pars; try dividing Par[2:3] by 10.")
+    elif exit_flag == -6:
+        print("Terminating because delta_min reached with a valid model.")
     elif exit_flag == -1:
-        print("Number of residuals in output of fun does not match supplied m. Exiting. ")
+        print("Number of residuals in output of fun does not match supplied m. Exiting.")
     elif exit_flag == 0:
-        print("g is sufficiently small")
+        print("g is sufficiently small.")
 
     return X, F, hF, exit_flag
