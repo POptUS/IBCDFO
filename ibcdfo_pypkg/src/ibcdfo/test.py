26490d142fe1fd7009b3f3e5294c93a9a425d95f
Jared O'Neal <joneal@anl.gov>
2025-05-16 11:55:03 -0500

    (Issue #173) test.py needs to return pass/fail result.

--- ibcdfo_pypkg/src/ibcdfo/test.py
+++ ibcdfo_pypkg/src/ibcdfo/test.py
@@ -9,12 +9,17 @@ def test(verbosity=1):
     using a simple text interface.
 
     This is included so that users can test their actual installation directly
-    or record test results in jupyter notebook output for reproducibility via
+    or record test results in jupyter notebook output for reproducibility
+    |via|::
+
                               ibcdfo.test()
 
-    Parameters
-        verbosity - the verbosity level to pass to the unittest TestRunner
+    :param verbosity: verbosity level to pass to the ``unittest``
+        ``TestRunner``
+    :return: True if all tests in package passed; False, otherwise.
     """
     loader = unittest.TestLoader()
     suite = load_tests(loader, None, None)
-    unittest.TextTestRunner(verbosity=verbosity).run(suite)
+    result = unittest.TextTestRunner(verbosity=verbosity).run(suite)
+
+    return result.wasSuccessful()
