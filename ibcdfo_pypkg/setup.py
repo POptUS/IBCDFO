c5117c7adfc5b087dba56296151f764a0fd0f8c5
Jared O'Neal <joneal@anl.gov>
2025-05-19 15:51:31 -0500

    (Issue #173) Fix up file that was not black-compliant.
    I left this in a poorly-formatted state to confirm that the Python standards
    checking action reported a failure.  It did.

--- ibcdfo_pypkg/setup.py
+++ ibcdfo_pypkg/setup.py
@@ -1,9 +1,7 @@
 import codecs
 from pathlib import Path
 
-from setuptools import (
-    setup, find_packages
-)
+from setuptools import setup, find_packages
 
 _PKG_ROOT = Path(__file__).resolve().parent
 
