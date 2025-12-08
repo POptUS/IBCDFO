675cea6cb207e61a364606d6536b97159df7abc4
Jared O'Neal <joneal@anl.gov>
2025-12-08 14:31:22 -0600

    (Issue #134) Intentionlly inject Sphinx config error.

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ copyright = "2023, The Regents of the University of California, " \
     "LLC through Argonne National Laboratory (subject to receipt of any " \
     "required approvals from the U.S. Dept. of Energy).  All rights reserved"
 author = "Jeffrey Larson, Matt Menickelly, and Stefan M. Wild"
-version = __version__
+versio = __version__
 release = version
 
 latex_packages = [
