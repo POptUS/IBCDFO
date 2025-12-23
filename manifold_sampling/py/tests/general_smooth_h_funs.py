308fe301a7c90deb39c3878e34898340a33b93f7
Jared O'Neal <joneal@anl.gov>
2025-12-23 10:36:24 -0600

    (Issue #226) Add missing MSP test to Python impl.
    This test exists already for the MATLAB impl.  Tests ran through locally.

---
+++ manifold_sampling/py/tests/general_smooth_h_funs.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def h_leastsquares(z, H0=None):
+    r"""
+    :math:`\hfun` function for constructing the standard least-squares objective
+    function
+
+    .. math::
+
+        f(\psp) = \hfun\left(\Ffun(\psp)\right)
+                = \sum_{i = 1}^{\nd} \Ffuncomp{i}(\psp)^2.
+
+    It is intended that this only be used for testing purposes.
+    """
+    n = len(z)
+    h = np.sum(z**2)
+    grads = np.reshape(2.0 * z, (n, 1))
+
+    if H0 is None:
+        return h, grads, [str(1)]
+
+    return np.array([h]), grads
