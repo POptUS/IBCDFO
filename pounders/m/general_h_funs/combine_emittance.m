ca9d1c843a45cfc874fb0061e5dd2017c41e348e
Jared O'Neal <joneal@anl.gov>
2026-01-06 08:38:04 -0600

    (Issue #226) Add assert back in.
    This aligns this function with its other related MATLAB and Python functions.

--- pounders/m/general_h_funs/combine_emittance.m
+++ pounders/m/general_h_funs/combine_emittance.m
@@ -2,7 +2,7 @@ function [G, H] = combine_emittance(Cres, Gres, Hres)
 
 [n, ~, m] = size(Hres);
 
-% assert(m==3, "Emittance calculation requires exactly three quantities");
+assert(m==3, "Emittance calculation requires exactly three quantities");
 
 G = Cres(1) * Gres(:, 2) + Cres(2) * Gres(:, 1) - 2 * Cres(end) * Gres(:, end);
 H = Cres(1) * Hres(:, :, 2) + Cres(2) * Hres(:, :, 1) + Gres(:, 2) * Gres(:, 1)' + Gres(:, 1) * Gres(:, 2)' - 2 * Cres(end) * Hres(:, :, end) - 2 * Gres(:, end) * Gres(:, end)';
