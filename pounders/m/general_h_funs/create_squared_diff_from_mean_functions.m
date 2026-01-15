273536b6fcf59a79d4941e7e0ab46ccbba72ea9e
Jared O'Neal <joneal@anl.gov>
2026-01-14 18:44:23 -0600

    (Issue #247) Remove whitespace.

--- pounders/m/general_h_funs/create_squared_diff_from_mean_functions.m
+++ pounders/m/general_h_funs/create_squared_diff_from_mean_functions.m
@@ -8,25 +8,25 @@ function [hfun, combinemodels] = create_squared_diff_from_mean_functions(alpha)
 
     function [G, H] = combine_squared_diff_from_mean(Cres, Gres, Hres, alpha)
         [n, ~, m] = size(Hres);
-        
+
         m_sumF = mean(Cres);
         m_sumG = 1 / m * sum(Gres, 2);
         sumH = sum(Hres, 3);
-        
+
         G = zeros(n, 1);
         for i = 1:m
             G = G + (Cres(i) - m_sumF) * (Gres(:, i) - m_sumG);
         end
         G = 2 * G - 2 * alpha * m_sumF * m_sumG;
-        
+
         H = zeros(n, n);
         for i = 1:m
             H = H + (Cres(i) - m_sumF) * (Hres(:, :, i) + sumH) + (Gres(:, i) - m_sumG) * (Gres(:, i) - m_sumG)';
         end
         H = 2 * H;
-        
+
         H = H - (2 * alpha / m) * m_sumF * sumH - (2 * alpha) * m_sumG * m_sumG';
-        
+
         % [grad, Hess] = matlab_symbolic_grad(Cres,Gres,Hres);
     end
 
