995f17cb5fd0e03d4eafd5d2d139e88aa65bf423
Jared O Neal <joneal@anl.gov>
2026-03-24 12:36:00 -0500

    (Issue #264) Tie MATLAB/POUNDERS to specific MINQ version as well.
    This is a the first step of freeing the MATLAB code from the MINQ submodule.

---
+++ pounders/m/check_minq_installation.m
@@ -0,0 +1,40 @@
+function [is_valid] = check_minq_installation(minq_version)
+    % Confirm that the folder containing the given MINQ version has been added
+    % to the MATLAB path and that the git clone that contains it is set to the
+    % required commit.
+    %
+    % An error is thrown if the installation cannot be found or is deemed
+    % invalid.
+    %
+    % Inputs:
+    %   minq_version - 5 to check for a valid the MINQ5 installation; 8, for a
+    %                  valid MINQ8 installation.
+
+    % ----- HARDCODED VALUES
+    % This must be the full git commit hash
+    VALID_MINQ_HASH = "7749b83645ea21e303a94e1200542f7028499bb8";
+
+    % ----- FIND MINQ CLONE INSTALLATION
+    % Users are required to add the specific folder containing their target
+    % version of MINQ.
+    if minq_version == 5
+        function_path = which("minqsw");
+        if function_path == ""
+            error("Please add /path/to/MINQ/m/minq5 to MATLAB path");
+        end
+    elseif minq_version == 8
+        function_path = which("minq8");
+        if function_path == ""
+            error("Please add /path/to/MINQ/m/minq8 to MATLAB path");
+        end
+    else
+        error(sprintf("Invalid MINQ version %d", minq_version));
+    end
+    [minq_path, ~, ~] = fileparts(function_path);
+    minq_repo = gitrepo(minq_path);
+
+    % ----- CONFIRM DESIRED VERSION
+    if minq_repo.LastCommit.ID ~= VALID_MINQ_HASH
+        error(sprintf("Please set MINQ clone to commit %s", VALID_MINQ_HASH));
+    end
+end
