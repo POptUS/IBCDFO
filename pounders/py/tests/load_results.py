52eb065cd985e21d5f3ed7b5d86f58d1028317ef
Jared O'Neal <joneal@anl.gov>
2026-06-03 13:53:30 -0500

    (Issue #291) One step closer to understanding scipy.io.loadmat weirdness.

--- pounders/py/tests/load_results.py
+++ pounders/py/tests/load_results.py
@@ -1,3 +1,5 @@
+import copy
+
 import scipy.io
 
 import numpy as np
@@ -11,21 +13,29 @@ def load_results(filename):
     """
     EXPECTED_KEYS = {"alg", "problem", "H", "Fvec", "X", "flag", "xk_best"}
 
-    # When adapting this to loading from MATLAB-generated files on GCE, I was
-    # forced to add the simplify_cells argument.  Without this, some loaded
-    # results had a "W" or a "V" field instead of the "X" field.  When I loaded
-    # those same files manually in Python using the exact same venv and
-    # scipy.io.loadmat(filename), I always saw the "X" field and didn't see the
-    # others.  I don't understand why this was happening nor why the addition
-    # of the flag solves it.
-    data = scipy.io.loadmat(filename, simplify_cells=True)
+    with open(filename, "rb") as fptr:
+        # If I call this function twice in a row on the same MATLAB-generated
+        # file (using both scipy v1.16.2 and v1.17.1), on some occasions the "X"
+        # field will appear with a different name (e.g., "W") on the second
+        # call.  This is apparently related to the change to xk_best below.  If
+        # I rerun that same test after exiting Python, I get the same result,
+        # which implies that the file itself is not being altered.  I hope that
+        # this is the case since we load in readonly mode.  This even happens if
+        # the two files loaded back-to-back are different.
+        #
+        # Therefore, I suspect that there is some sort of caching of or
+        # accidental referencing to loaded data.  Caching does not make sense
+        # when loading two different files.  However, using a deepcopy resolves
+        # the issue.  A simple copy is not sufficient, which makes sense since
+        # data is a nested dictionary.
+        data = copy.deepcopy(scipy.io.loadmat(filename))
     keys = [k for k in data.keys() if not k.startswith("__")]
     assert set(keys) == EXPECTED_KEYS
 
-    algorithm = str(data["alg"])
+    algorithm = str(np.squeeze(data["alg"]))
     assert algorithm in ["POUNDERS_M", "POUNDERS_Py"]
 
-    problem = str(data["problem"])
+    problem = str(np.squeeze(data["problem"]))
     if (not problem.startswith("problem")) or (not problem.endswith("from More/Wild")):
         raise ValueError(f"Invalid problem spec ({problem})")
     try:
@@ -33,7 +43,7 @@ def load_results(filename):
     except Exception:
         raise ValueError(f"Invalid problem spec ({problem})")
 
-    H = data["H"]
+    H = np.atleast_1d(np.squeeze(data["H"]))
     assert H.ndim == 1
     n_evaluations = len(H)
     assert all(np.isreal(H))
@@ -57,10 +67,10 @@ def load_results(filename):
     assert all(np.isreal(X.flatten()))
     assert all(np.isfinite(X.flatten()))
 
-    flag = data["flag"]
+    flag = np.squeeze(data["flag"])
     assert (flag >= 0.0) or (flag in [-6, -5, -4, -3, -2, -1])
 
-    xk_best = data["xk_best"]
+    xk_best = np.squeeze(data["xk_best"])
     if algorithm == "POUNDERS_M":
         # The MATLAB implementation's test suite saves the index of the best
         # approximation as a 1-based integer.  However, we need to adjust it to
