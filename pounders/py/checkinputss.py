ffb15aa1b2a0d2e2e12a4fd0ea92219648ebdc81
Jared O'Neal <joneal@anl.gov>
2026-08-12 15:08:41 -0500

    (Issue #317) Clean-up checkinputss a bit.

--- pounders/py/checkinputss.py
+++ pounders/py/checkinputss.py
@@ -34,8 +34,7 @@ def checkinputss(Ffun, X_0, n, np_max, nf_max, g_tol, delta_0, nfs, m, X_init, F
     if not isinstance(np_max, numbers.Integral):
         raise TypeError(f"Error: np_max is not an integer ({np_max})")
     if np_max < n + 1 or np_max > int(0.5 * (n + 1) * (n + 2)):
-        np_max = max(n + 1, min(np_max, int(0.5 * (n + 1) * (n + 2))))
-        raise ValueError("Error: np_max not in [n+1, 0.5 * (n+1) * (n+2)]")
+        raise ValueError(f"Error: np_max ({np_max}) not in [n+1, 0.5 * (n+1) * (n+2)]")
 
     # Check standard positive quantities
     if not isinstance(nfs, numbers.Integral):
@@ -43,12 +42,13 @@ def checkinputss(Ffun, X_0, n, np_max, nf_max, g_tol, delta_0, nfs, m, X_init, F
     if nfs < 0:
         raise ValueError(f"Error: nfs is not positive integer ({nfs})")
 
-    # nf_max is the actual evaluations to be made during the optimization.
-    # There is no sense in running the optimizatio without at least one
-    # evaluation beyond the n+1 geometry points.
+    # nf_max is the max actual evaluations to be made during an optimization.
+    # There is no sense in running an optimization without evaluating at least
+    # once at a non-geometry point chosen by POUNDERS.  Even if calling code
+    # provides more than n+1 values, we still let POUNDERS choose one point.
     if not isinstance(nf_max, numbers.Integral):
         raise TypeError(f"Error: nf_max is not an integer ({nf_max})")
-    min_required_evals = np.max(((n + 1) - nfs, 1))
+    min_required_evals = np.max((n - nfs + 2, 1))
     if nf_max < min_required_evals:
         raise TypeError(f"Error: max number of evaluations ({nf_max}) should be >= {min_required_evals}")
 
@@ -68,18 +68,14 @@ def checkinputss(Ffun, X_0, n, np_max, nf_max, g_tol, delta_0, nfs, m, X_init, F
 
     # Check the bounds
     if not isinstance(Low, np.ndarray):
-        raise TypeError("Error: Low must be Numpy array")
-    if Low.ndim != 1:
-        raise ValueError("Error: Low must be 1D Numpy array")
-    if len(Low) != n:
+        raise TypeError("Error: Low must be 1D Numpy array")
+    if (Low.ndim != 1) and (len(Low) != n):
         raise ValueError(f"Error: Low is not {n} element 1D Numpy array")
     if np.any(np.isnan(Low)):
         raise ValueError("Error: Low contain non-finite values")
     if not isinstance(Upp, np.ndarray):
-        raise TypeError("Error: Upp must be Numpy array")
-    if Upp.ndim != 1:
-        raise ValueError("Error: Upp must be 1D Numpy array")
-    if len(Upp) != n:
+        raise TypeError("Error: Upp must be 1D Numpy array")
+    if (Upp.ndim != 1) and (len(Upp) != n):
         raise ValueError(f"Error: Upp is not {n} element 1D Numpy array")
     if np.any(np.isnan(Upp)):
         raise ValueError("Error: Upp contain non-finite values")
@@ -88,7 +84,7 @@ def checkinputss(Ffun, X_0, n, np_max, nf_max, g_tol, delta_0, nfs, m, X_init, F
 
     # Check starting point
     if not isinstance(X_0, np.ndarray):
-        raise TypeError("Error: X_0 must be a Numpy array")
+        raise TypeError("Error: X_0 must be a 1D Numpy array")
     elif (X_0.ndim != 1) or (len(X_0) != n):
         raise ValueError(f"Error: X_0 is not an {n} element 1D Numpy array")
     if any(X_0 < Low) or any(X_0 > Upp):
@@ -96,18 +92,16 @@ def checkinputss(Ffun, X_0, n, np_max, nf_max, g_tol, delta_0, nfs, m, X_init, F
 
     # Check prior evaluations
     if not isinstance(xk_in, numbers.Integral):
-        raise TypeError("Error: starting point index not an integer")
+        raise TypeError(f"Error: starting point index not an integer ({xk_in})")
     elif (xk_in < 0) or ((nfs == 0) and (xk_in != 0)) or ((nfs > 0) and (xk_in >= nfs)):
-        raise ValueError("Error: Invalid starting point index")
+        raise ValueError(f"Error: Invalid starting point index ({xk_in})")
 
     if not isinstance(X_init, np.ndarray):
         raise TypeError("Error: X_init must be a 2D Numpy array")
     if X_init.ndim != 2:
         raise ValueError("Error: X_init must be a 2D Numpy array")
-    if X_init.shape[0] != nfs:
-        raise ValueError("Error: number of initial points nfs does not match input X_init")
-    elif X_init.shape[1] != n:
-        raise ValueError("Error: X_init does not contain the right number of coordinates")
+    if X_init.shape != (nfs, n):
+        raise ValueError(f"Error: Invalid X_init shape {X_init.shape}.  Expect ({nfs},{n})")
     if nfs > 0:
         if not np.all(np.isfinite(X_init)):
             raise ValueError("Error: X_init contains non-finite values")
@@ -118,10 +112,8 @@ def checkinputss(Ffun, X_0, n, np_max, nf_max, g_tol, delta_0, nfs, m, X_init, F
         raise TypeError("Error: F_init must be a 2D Numpy array")
     if F_init.ndim != 2:
         raise ValueError("Error: F_init must be a 2D Numpy array")
-    if F_init.shape[0] != nfs:
-        raise ValueError("Error: number of starting f values nfs does not match input F_init")
-    elif F_init.shape[1] != m:
-        raise ValueError("Error: F_init does not contain the right number of residuals")
+    if F_init.shape != (nfs, m):
+        raise ValueError(f"Error: Invalid F_init shape {F_init.shape}.  Expect ({nfs},{m})")
     if nfs > 0:
         if not np.all(np.isfinite(F_init)):
             raise ValueError("Error: F_init contains non-finite values")
