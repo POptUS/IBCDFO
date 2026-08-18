5396438d53fcf6fb8dae02453cea8f2ed2bfc51d
Jared O'Neal <joneal@anl.gov>
2026-08-18 16:19:55 -0500

    (Issue #317) Cleaning as part of PR review.

--- pounders/py/tests/TestCheckInputss.py
+++ pounders/py/tests/TestCheckInputss.py
@@ -10,7 +10,7 @@ import numpy as np
 from ibcdfo.pounders.checkinputss import checkinputss
 
 
-class TestPoundersInterface(unittest.TestCase):
+class TestCheckInputss(unittest.TestCase):
     def setUp(self):
         self.__ERROR_HDR = "Error: "
         self.__NOT_INT = (None, "", 1.0, 1.1, {}, [1], {1})
@@ -40,12 +40,14 @@ class TestPoundersInterface(unittest.TestCase):
         checkinputss(**self.__KWARGS)
 
     def testN(self):
-        # Prefer testing checkinputss() by testing direct calls to pounders.py.
-        # POUNDERS error checks n before calling checkinputss() because n is
-        # used to set other local variables to be checked.  Therefore, we test
-        # here in case this functionality is actually used at some point.
+        # Prefer testing checkinputss() indirectly by testing calls to
+        # pounders.py.  POUNDERS error checks n before calling checkinputss()
+        # because n is used to set other local variables that also need to be
+        # checked.  Therefore, error checks of n in checkinputss are never
+        # exercised, and  we test here in case this functionality is actually
+        # used at some point.
+        kwargs = copy.deepcopy(self.__KWARGS)
         for bad in self.__NOT_INT:
-            kwargs = copy.deepcopy(self.__KWARGS)
             kwargs["n"] = bad
             with self.assertRaises(TypeError) as err:
                 checkinputss(**kwargs)
@@ -53,7 +55,6 @@ class TestPoundersInterface(unittest.TestCase):
             # print(err_msg)
             self.assertTrue(err_msg.startswith(self.__ERROR_HDR))
         for bad in [-10, -1, 0]:
-            kwargs = copy.deepcopy(self.__KWARGS)
             kwargs["n"] = bad
             with self.assertRaises(ValueError) as err:
                 checkinputss(**kwargs)
@@ -63,8 +64,8 @@ class TestPoundersInterface(unittest.TestCase):
 
     def testM(self):
         # See notes for testN().
+        kwargs = copy.deepcopy(self.__KWARGS)
         for bad in self.__NOT_INT:
-            kwargs = copy.deepcopy(self.__KWARGS)
             kwargs["m"] = bad
             with self.assertRaises(TypeError) as err:
                 checkinputss(**kwargs)
@@ -72,7 +73,6 @@ class TestPoundersInterface(unittest.TestCase):
             # print(err_msg)
             self.assertTrue(err_msg.startswith(self.__ERROR_HDR))
         for bad in [-10, -1, 0]:
-            kwargs = copy.deepcopy(self.__KWARGS)
             kwargs["m"] = bad
             with self.assertRaises(ValueError) as err:
                 checkinputss(**kwargs)
