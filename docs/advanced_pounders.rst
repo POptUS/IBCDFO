1f12d658fa5850476852e39913cf7c5c070c43b8
Jared O'Neal <joneal@anl.gov>
2026-08-31 10:20:53 -0500

    (Issue #291) Improve advanced POUNDERS API docs.

--- docs/advanced_pounders.rst
+++ docs/advanced_pounders.rst
@@ -1,10 +1,33 @@
 Advanced |pounders| Interface
 =============================
 
+Trust-region subproblem solver
+------------------------------
+For both Python and |matlab|, Arnold Neumaier’s minq5 solver is used by default
+in |pounders| to solve trust-region subproblems.  This has been the default
+solver for a very long time.
+
+While ``ibcdfo.pounders.pounders.pounders`` does allow users to provide their
+own subproblem solver, users can also provide one of potentially many solvers
+officially provided by |ibcdfo| with the ``create_trsp_solver`` functions
+documented below.  Users that wish to provide an external solver should refer to
+that documentation to understand |pounders|'s interface requirements.
+
 Python
-------
+^^^^^^
 .. autofunction:: ibcdfo.pounders.create_trsp_solver
 
 |matlab|
---------
+^^^^^^^^
 .. mat:autofunction:: pounders.m.create_trsp_solver
+
+High-level interface
+--------------------
+The following is a prototype of a potential high-level user interface for
+|pounders|.  Since it's interface is minimal and contains only those arguments
+that must be or are likely to be supplied by typical users, it could replace
+``ibcdfo.run_pounders``.  If so, then the low-level interface
+``ibcdfo.pounders.pounders.pounders`` would be left in the public interface for
+power users.
+
+.. autofunction:: ibcdfo.pounders._run_user_friendly.run_user_friendly
--- pounders/py/_run_user_friendly.py
+++ pounders/py/_run_user_friendly.py
@@ -12,17 +12,18 @@ def run_user_friendly(Ffun, X_0, n, nf_max, g_tol, delta_0, m, Low, Upp, hfun=No
     r"""
     Run |pounders| on the optimization problem specified by the given arguments.
 
-    :param Ffun:    Function that returns :math:`\Ffun(\psp)` as :math:`\nd`
-        element NumPy array for given :math:`\psp`
-    :param X_0:     :math:`\np` element NumPy array that specifies the initial
-        point
+    :param Ffun:    Function that returns :math:`\Ffun(\psp)` as
+        **m**-element NumPy array for given :math:`\psp`
+    :param X_0:     **n**-element 1D NumPy array that specifies the
+        initial point, which must satisfy the boundary constraints
     :param n:       Dimension (number of continuous, real-valued input variables)
-    :param nf_max:  Maximum number of function evaluations (:math:`> \np+1`)
+    :param nf_max:  Maximum number of function evaluations (> **n** + 1 if
+        **Prior** not provided or **nfs** = 0; :math:`\ge` **n** + 1, if **nfs** > 0)
     :param g_tol:   Tolerance for the 2-norm of the model gradient
     :param delta_0: Positive initial trust region radius
-    :param m:       Dimension of output of ``Ffun`` (number of component functions)
-    :param Low:     :math:`\np` element NumPy array of lower bounds
-    :param Upp:     :math:`\np` element NumPy array of upper bounds
+    :param m:       Dimension of output of **Ffun** (number of component functions)
+    :param Low:     **n**-element 1D NumPy array of lower bounds
+    :param Upp:     **n**-element 1D NumPy array of upper bounds
     :param hfun: ``dict`` that defines objective function :math:`f` to use.
         Set to ``None`` to use the default
         :py:func:`ibcdfo.pounders.h_leastsquares` hfun function.
@@ -31,33 +32,39 @@ def run_user_friendly(Ffun, X_0, n, nf_max, g_tol, delta_0, m, Low, Upp, hfun=No
           :math:`\Ffun(\psp)` to scalars for minimization
         * **combinemodels** - Function that maps the linear and quadratic terms
           from the models of :math:`\Ffun` into a single quadratic model
-    :param Prior:   ``dict`` describing  past evaluations of ``Ffun``.  Set to
-        ``None`` to run optimization assuming no past evaluations. A nonempty
-        **Prior** must contain entries:
+
+    :param Prior:   ``dict`` describing past evaluations of **Ffun**.  Set to ``None``
+        to run optimization assuming no past evaluations. A nonempty **Prior** must
+        contain entries:
 
         * **nfs** - Number of past function evaluations
-        * **X_init** - :math:`\mathrm{nfs} \times \np` NumPy array of points
-          :math:`\psp_k`
-        * **F_init** - :math:`\mathrm{nfs} \times \nd` NumPy array of values
-          :math:`\Ffun(\psp_k)` computed with ``Ffun``
-        * **xk_in** -  Zero-based index into ``X_init`` and ``F_init`` that
-          corresponds to the point and value to use as initial point for
-          optimization. Note that if **Prior** is nonempty, this will override
-          the previously specified **X_0**.
-    :param concurrent: Set to True if ``Ffun`` is parallelized and you would
+        * **X_init** - **nfs** :math:`\times` **n** NumPy array of distinct
+          points :math:`\psp_i`
+        * **F_init** - **nfs** :math:`\times` **m** NumPy array of values
+          :math:`\Ffun(\psp_i)` computed with **Ffun**
+        * **xk_in** - Zero-based index into **X_init** and **F_init** that
+          corresponds to the point and value to use as the initial point for
+          optimization. Note that if **Prior** is nonempty, **X_init[xk_in]**
+          and **X_0** must be identical and still satisfy the boundary
+          constraints.
+
+    :param concurrent: Set to True if **Ffun** is parallelized and you would
         like |pounders| to make use of that potential performance increase.
 
     :return:
-        * **X** - :math:`\mathrm{nf\_max+nfs}\times \np` NumPy array containing
-          locations of evaluated points in the order in which they were
-          evaluated
-        * **F** - :math:`\mathrm{nf\_max+nfs}\times \nd` NumPy array containing
-          the function values at ``X`` with matching ordering
-        * **hF** - :math:`\mathrm{nf\_max+nfs}\times 1` Composed values
-          ``hfun(Ffun(x))`` for evaluated points ``x`` in ``X``
-        * **flag** - Termination criteria flag (See general |pounders| documentation)
-        * **xk_in** - Zero-based index of point in ``X`` representing
-          incumbent at termination (approximate local minimizer if `flag=0`)
+        * **X** - :math:`k \times` **n** NumPy array containing all points of
+          evaulation :math:`\psp_i` (including those provided in **Prior**) in
+          the order in which they were evaluated, where **nfs** :math:`< k \le`
+          **nf_max** + **nfs**.
+        * **F** - :math:`k \times` **m** NumPy array of values :math:`\Ffun(\psp_i)`
+          computed with **Ffun** at all :math:`\psp_i` in  **X** and provided
+          with matching ordering.
+        * **hF** - :math:`k`-element 1D NumPy array of composed values
+          :math:`\hfun(\Ffun(\psp_i))` computed with **hfun** and **Ffun** at
+          all :math:`\psp_i` in  **X** and provided with matching ordering.
+        * **flag** - Termination criteria flag (see general |pounders| documentation)
+        * **xk_in** - Zero-based index of point in **X** representing the
+          incumbent at termination (approximate local minimizer if ``flag=0``)
     """
     # ----- CHOOSE DEFAULT VALUES ON-BEHALF OF USERS
     # All non-power users should use the MINQ5 TRSP, which implies that all
